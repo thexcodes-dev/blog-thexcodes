0d703ad382ad0f681ce7a645c9ee0013dade59ca
Leandro Oliveira <leandroti.oliveira@gmail.com>
2022-10-28 09:41:41 -0300

    add google analytcs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import { ApolloProvider } from "@apollo/client";
 import { ChakraProvider } from '@chakra-ui/react'
+import Script from 'next/script';
 
 import { client } from '../service/apollo';
 import { theme } from '../styles/theme';
@@ -8,11 +9,24 @@ import { theme } from '../styles/theme';
 function MyApp({ Component, pageProps, router }: AppProps) {
 
   return (
-    <ApolloProvider client={client}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </ApolloProvider>
+    <>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+      <Script strategy="lazyOnload" id="googleAnalytics">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+              });
+          `}
+      </Script>    
+      <ApolloProvider client={client}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </ApolloProvider>
+    </>
   )
 }
 
